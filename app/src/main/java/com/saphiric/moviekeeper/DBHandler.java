038ccaae29fc251665a25973eae88f3c1eec2669
Darrell <sinner080292@gmail.com>
2014-11-05 17:35:14 -0500

    Setup the Delete Handler
    Setup a handler method for deleting entries.  Fixed issue w/ the search query string

--- app/src/main/java/com/saphiric/moviekeeper/DBHandler.java
+++ app/src/main/java/com/saphiric/moviekeeper/DBHandler.java
@@ -58,7 +58,7 @@ public class DBHandler extends SQLiteOpenHelper {
 }
 
 	public Movie findMovie(String movietitle) {
-	String query = "Select * FROM " + TABLE_MOVIES + " WHERE " + COLUMN_MOVIETITLE + " =  \"" + productname + "\"";
+	String query = "Select * FROM " + TABLE_MOVIES + " WHERE " + COLUMN_MOVIETITLE + " =  \"" + movietitle + "\"";
 	
 	SQLiteDatabase db = this.getWritableDatabase();
 	
@@ -78,6 +78,29 @@ public class DBHandler extends SQLiteOpenHelper {
 	}
 	db.close();
 	return movie;
+}
+
+public boolean deleteMovie(String movietitle) {
+	
+	boolean result = false;
+	
+	String query = "Select * FROM " + TABLE_MOVIES + " WHERE " + COLUMN_MOVIETITLE + " =  \"" + movietitle + "\"";
+
+	SQLiteDatabase db = this.getWritableDatabase();
+	
+	Cursor cursor = db.rawQuery(query, null);
+	
+	Movie movie = new Movie();
+	
+	if (cursor.moveToFirst()) {
+		movie.setID(Integer.parseInt(cursor.getString(0)));
+		db.delete(TABLE_PRODUCTS, COLUMN_ID + " = ?",
+	            new String[] { String.valueOf(movie.getID()) });
+		cursor.close();
+		result = true;
+	}
+	db.close();
+	return result;
 }
 
 }
